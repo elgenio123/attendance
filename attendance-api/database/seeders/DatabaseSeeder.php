06eb4be7a3067ea260ce56d2bf609516ab5b7fb6
elgenio <rickentchabet@gmail.com>
2025-09-09 22:41:50 +0100

    Edit deploy script for creation of user

--- attendance-api/database/seeders/DatabaseSeeder.php
+++ attendance-api/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            AttendanceSeeder::class,
+        User::create([
+            'name' => 'Test Teacher',
+            'email' => 'testteacher@example.com',
+            'password' => Hash::make('password'), // Use a secure password
+            'user_type' => 'teacher', // Assuming there is an 'is_admin' column in the users table
+            'registration_number' => '117320'
+        ]);
+        User::create([
+            'name' => 'Test Student',
+            'email' => 'teststudent@example.com',
+            'password' => Hash::make('password'), // Use a secure password
+            'user_type' => 'student', // Assuming there is an 'is_admin' column in the users table
+            'registration_number' => '117321'
         ]);
     }
 }
